0edfe42b15df520302c657efd38a1b23bd7c4d94
Everton Vicente <everton.vicente@onfriday.com.br>
2022-02-01 10:28:11 -0300

    Finalizacao da Sação 16: Topicos especiais em C#

---
+++ Extensions_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Extensions_Methods;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando dia
+        DateTime dt = DateTime.Now.AddDays(-5);
+
+        Console.WriteLine(dt.ElapsedTime());
+
+        // Testando dia
+        DateTime dt1 = DateTime.Now.AddHours(-5);
+
+        Console.WriteLine(dt1.ElapsedTime());
+
+        Console.WriteLine("\n-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\n");
+
+        string s1 = "Good Morning dear students!";
+
+        Console.WriteLine("Sem cortar: " + s1);
+        Console.WriteLine("Cortando até o 10 caracter: " + s1.Cut(10));
+    }
+}
